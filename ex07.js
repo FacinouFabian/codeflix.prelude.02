702b29f10bf339becd5c98534307795d39a7d4f0
FacinouFabian <fabianfacinou02@gmail.com>
2019-10-23 22:58:02 +0200

    'projet'

--- ex07.js
+++ ex07.js
@@ -1,16 +1,16 @@
-module.exports = function intersection(...values){
-    for (ent of values){
-        // set a first tab in $first, and the others tabs
-        [first, ...rest] = [...values]
-        // search elements in first
-        for (elt of first){
-            if(includes(elt)){
-
-            }
-        }
-        // if ...rest includes elements of first
-
-        // return the first tab
-        return first
-    }
+module.exports = function intersection(...arrays){
+   let newArr = []
+   let [first, ...rest] = arrays
+   for(let elt of first){
+       let present = true
+       for(const other of rest){
+           if(!other.includes(elt)){
+               present=false
+           }
+       }
+       if(present){
+           newArr.push(elt)
+       }
+   }
+   return newArr
 }
